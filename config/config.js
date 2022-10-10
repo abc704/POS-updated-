757b15a0d827034b5d7e7f3414d8ad9d2d073e23
Shashank kumar <56788944+abc704@users.noreply.github.com>
2022-10-10 14:32:40 +0530

    Added a Procfile.

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ require("colors");
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(`mongodb+srv://shashank:HK2RJ2gQyl7MqBYN@cluster0.ouqeamn.mongodb.net/Netflix?retryWrites=true&w=majority`);
     console.log(`MongoDB Connected ${conn.connection.host}`.bgYellow);
   } catch (error) {
     console.log(`Error : ${error.message}`.bgRed);
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/bills", require("./routes/billsRoute"));
 
 //port
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 //listen
 app.listen(PORT, () => {
